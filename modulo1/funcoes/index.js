7537b533cdc1e6573b8946b4be29b2e896a0ea92
JuanPeluso <juan.darwill@gmail.com>
2022-05-12 16:53:17 -0300

    atividade incompleta do dia de hoje

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,58 @@
+/* Atividade Leitura 1
+
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2)) - Aparecerá na tela o resultado da multiplicação 2*5 = 10
+console.log(minhaFuncao(10)) - Aparecerá na tela o resultado da multiplicação 10*5 = 50 
+- Retirado os console(log) do código nada será impresso.
+*/
+
+/* Atividade Leitura 2
+
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta) - Função retorna true or false dependendo se texto inserido contenha palavra cenoura ou não.
+As 3 opções de texto inserido dá true */
+
+// Atividade de Escrita 1
+
+/*function olaAtividade(){
+let nome = "Juan"
+let idade = "30"
+let cidade = "Santos"
+let souEstudante = "estudante"
+
+return `Olá ${nome}, você tem ${idade} anos, mora em ${cidade} e é um ${souEstudante}.`
+}
+
+let resposta = olaAtividade()
+console.log(resposta)
+
+function pesquisaUser(){
+let nomeUser = prompt("Qual o seu nome?")
+let idadeUser = Number(prompt("Qual a sua idade?"))
+let cidadeUser = prompt("Mora em qual cidade?")
+let profissaoUser = prompt("Qual sua profissão?")
+
+return `Eu sou ${nomeUser}, tenho ${idadeUser} anos, moro em ${cidadeUser} e sou ${profissaoUser}`
+
+}
+
+let respostaUser = pesquisaUser()
+console.log(respostaUser) */
+
+//Atividade de Escrita 2
+
+function somaTotal(){
+const numberOne = Number(prompt("Digite um número:"))
+const numberTwo = Number(prompt("Digite outro número:"))
+const somaDois = function (numberOne, numberTwo)
+
+}
